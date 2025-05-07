527bd4b698888aaa76cedccffa1030b12e6de8fb
helcanlalen <hellenlacan1994@gmail.com>
2025-05-06 21:12:54 -0600

    add file

--- src/main/java/org/example/route/ConfigurationProvider.java
+++ src/main/java/org/example/route/ConfigurationProvider.java
@@ -22,10 +22,10 @@ public class ConfigurationProvider {
         try {
             // Lee el archivo de la raíz del proyecto
             System.out.println("Directorio de trabajo actual: " + System.getProperty("user.dir"));
-            File file = new File("application.properties");
+            File file = new File("applicationTransformada.properties");
             System.out.println("¿Existe el archivo en la ruta actual?: " + file.exists());
             System.out.println("Ruta absoluta del archivo buscado: " + file.getAbsolutePath());
-            properties.load(new FileInputStream("application.properties"));
+            properties.load(new FileInputStream("applicationTransformada.properties"));
         } catch (IOException e) {
             System.err.println("No se pudo cargar application.properties: " + e.getMessage());
         }

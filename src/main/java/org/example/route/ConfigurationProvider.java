6f7bdfa2f2505d4fc54d1f81d4c61b793c64725e
helcanlalen <hellenlacan1994@gmail.com>
2025-05-06 20:55:38 -0600

    buscando archivo properties

--- src/main/java/org/example/route/ConfigurationProvider.java
+++ src/main/java/org/example/route/ConfigurationProvider.java
@@ -10,6 +10,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import java.io.File; 
+
 @ApplicationScoped
 public class ConfigurationProvider {
     
@@ -19,12 +21,10 @@ public class ConfigurationProvider {
         properties = new Properties();
         try {
             // Lee el archivo de la raíz del proyecto
-
             System.out.println("Directorio de trabajo actual: " + System.getProperty("user.dir"));
             File file = new File("application.properties");
             System.out.println("¿Existe el archivo en la ruta actual?: " + file.exists());
             System.out.println("Ruta absoluta del archivo buscado: " + file.getAbsolutePath());
-
             properties.load(new FileInputStream("application.properties"));
         } catch (IOException e) {
             System.err.println("No se pudo cargar application.properties: " + e.getMessage());

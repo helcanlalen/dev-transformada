9e0e8744f2af8875ac7311054e2d0163bd3acbf1
Hellen Hdz <hellenlacan1994@gmail.com>
2025-05-12 16:37:04 -0600

    Update KafkaToLogRoute.java
    update nominas con convenio

--- src/main/java/org/example/route/KafkaToLogRoute.java
+++ src/main/java/org/example/route/KafkaToLogRoute.java
@@ -77,7 +77,7 @@ public class KafkaToLogRoute extends RouteBuilder {
             .setBody(simple("{\"error\": \"${exception.message}\"}"));
             
         // HTTP endpoint that processes requests and sends them to Kafka
-        from("platform-http:test")
+        from("platform-http:/nominas-con-convenio")
             .routeId("http-to-kafka")
             // Generate correlation ID for request tracking
             .process(exchange -> {

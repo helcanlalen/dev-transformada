9b6758771ce7402c9da88e11bcc0fda8af5938e4
Hellen Hdz <hellenlacan1994@gmail.com>
2025-05-12 18:49:28 -0600

    Update ConfigurationProvider.java

--- src/main/java/org/example/route/ConfigurationProvider.java
+++ src/main/java/org/example/route/ConfigurationProvider.java
@@ -20,25 +20,19 @@ public class ConfigurationProvider {
     public ConfigurationProvider() {
         properties = new Properties();
         try {
-            // Lee el archivo de la raíz del proyecto
-            System.out.println("Directorio de trabajo actual: " + System.getProperty("user.dir"));
-            File file = new File("application.properties");
-            System.out.println("¿Existe el archivo en la ruta actual?: " + file.exists());
-            System.out.println("Ruta absoluta del archivo buscado: " + file.getAbsolutePath());
-
+            // Lectura variables de enterno
             String kafkaTopicRequest = System.getenv("KAFKA_TOPIC_REQUEST");
             String kafkaTopicRequestPlanilla = System.getenv("KAFKA_TOPIC_REQUEST_PLANILLA");
             String port = System.getenv("PORT");
 
             // Mostrar los valores para depuración
-            System.out.println("📋 Configuración cargada desde variables de entorno:");
-            System.out.println("   🔹 kafka_topic_request = " + kafkaTopicRequest);
-            System.out.println("   🔹 kafka_topic_request_planilla = " + kafkaTopicRequestPlanilla);
-            System.out.println("   🔹 port = " + port);
+            System.out.println("Configuración cargada desde variables de entorno:");
+            System.out.println("kafka_topic_request = " + kafkaTopicRequest);
+            System.out.println("kafka_topic_request_planilla = " + kafkaTopicRequestPlanilla);
+            System.out.println("port = " + port);
             
-            properties.load(new FileInputStream("application.properties"));
         } catch (IOException e) {
-            System.err.println("No se pudo cargar application.properties: " + e.getMessage());
+            System.err.println("No se puden cargar las variables de entorno: " + e.getMessage());
         }
     }
     

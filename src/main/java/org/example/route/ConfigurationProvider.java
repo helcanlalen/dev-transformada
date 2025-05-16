b7c7dd840e8f5b7be63683a0a5c8f4a31a88ae44
Hellen Hdz <hellenlacan1994@gmail.com>
2025-05-16 13:48:34 -0600

    Update ConfigurationProvider.java

--- src/main/java/org/example/route/ConfigurationProvider.java
+++ src/main/java/org/example/route/ConfigurationProvider.java
@@ -21,23 +21,29 @@ public class ConfigurationProvider {
     String port; 
     String cluster;
     String cluster_port;
+    String productIdNomina;
+    String productIdLibreDisp;
     
     public ConfigurationProvider() {
         properties = new Properties();
 
-        // Lectura variables de enterno
+        //Variables de entorno
         kafkaTopicRequest = System.getenv("KAFKA_TOPIC_REQUEST");
         kafkaTopicRequestNomina = System.getenv("KAFKA_TOPIC_REQUEST_RETAIL");
         port = System.getenv("PORT");
         cluster = System.getenv("CLUSTER");
         cluster_port = System.getenv("CLUSTER_PORT");
-        // Mostrar los valores para depuración
-        System.out.println("Configuración cargada desde variables de entorno:");
+        productIdNomina = System.getenv("ID_PROD_NOMINA");
+        productIdLibreDisp = System.getenv("ID_PROD_LIB_DISP");
+
+        System.out.println("Configuracion cargada desde variables de entorno:");
         System.out.println("kafka_topic_request = " + kafkaTopicRequest);
         System.out.println("kafka_topic_request_Nomina = " + kafkaTopicRequestNomina);
         System.out.println("port = " + port);
         System.out.println("cluster = " + cluster);        
         System.out.println("cluster_port = " + cluster_port);
+        System.out.println("Product ID nomina = " + productIdNomina);        
+        System.out.println("Product ID libre disponibilidad= " + productIdLibreDisp);
     }
     
     @Produces
@@ -62,5 +68,13 @@ public class ConfigurationProvider {
     
     public String getClusterPort() {
         return this.cluster_port;
+    }
+
+    public String getProductIdNomina() {
+        return this.productIdNomina;
+    }    
+    
+    public String getproductIdLibreDisp() {
+        return this.productIdLibreDisp;
     }
 }
